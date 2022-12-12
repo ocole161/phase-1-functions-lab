6dcc0e5ef6e37a7b2948971b76a337a8877b16dd
Owen Cole <ocole161@gmail.com>
2022-12-12 15:59:46 -0700

    commit tree 5608968676d7d6b3a2796a050e7b2b81019c0568

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(42 - pickupLocation)
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let feetTravelled = distanceTravelledInFeet(start,destination)
+    if (feetTravelled < 400) {
+        return 0
+    }
+    if (feetTravelled > 2500) {
+        return 'cannot travel that far'
+    }
+    if (feetTravelled > 2000) {
+        if (feetTravelled < 2500) {
+            return 25
+        }
+    }
+
+    return (distanceTravelledInFeet(start,destination) - 400) * 0.02
+}
